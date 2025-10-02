c425270090f8b032e6c1cad8d114d49ace28e7ac
nmarkakis17 <nmarkakis@towson.edu>
2025-10-02 08:05:53 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Registration from './pages/Registration.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+<Routes>
+  <Route path="/" element={<App />} />
+  <Route path="/register" element={<Registration />} />
+</Routes>
