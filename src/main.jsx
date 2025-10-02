5319a1ade0050237b19657ee6194c49f84a52198
nmarkakis17 <nmarkakis@towson.edu>
2025-10-02 09:27:31 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
 // src/main.jsx
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import Registration from './pages/Registration.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import Registration from "./pages/Registration.jsx"; // ✅ adjust if you put it elsewhere
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        {/* Landing (App) */}
         <Route path="/" element={<App />} />
+
+        {/* Registration */}
         <Route path="/register" element={<Registration />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
