bff99f3ddbcc50c22b4822072891d10d9f1e1bbf
nmarkakis17 <nmarkakis@towson.edu>
2025-10-03 13:26:22 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
-// src/main.jsx
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import App from "./App.jsx"
-
-const root = document.getElementById("root")
-if (!root) {
-  // super defensive: create a root if index.html was altered
-  const div = document.createElement("div")
-  div.id = "root"
-  document.body.appendChild(div)
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
